7aa63ae61f3e867304bd645b8e755dc0131a1dc8
Amit <www.amitkewot@gmail.com>
2022-09-07 01:18:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import {Header} from "./components/Header/Header";
+import Body from './components/Body/Body'
+function App() {
+  return (
+    <div className='App'>  
+      <Header/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
